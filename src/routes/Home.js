8070727a10dabef937e633e61f468f7905c3503d
jsky119 <87289235+jsky119@users.noreply.github.com>
2021-11-15 18:49:45 +0900

    Update Home.js styled components

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { gql } from "@apollo/client";
 import { useQuery } from "@apollo/client";
+import styled from "styled-components";
+import Movie from "../components/Movie";
 
 const GET_MOVIES = gql`
   {
@@ -11,12 +13,56 @@ const GET_MOVIES = gql`
   }
 `;
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const Header = styled.header`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 40vh;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  font-weght: bold;
+  margin-bottom: 30px;
+`;
+
+const Subtitle = styled.h3`
+  font-size: 30px;
+`;
+
+const Loading = styled.div`
+  margin-top: 10px;
+`;
+
+const Movies = styled.div``;
+
 export default () => {
-  const { loading, error, data } = useQuery(GET_MOVIES);
-  if (loading) {
-    return "loading...";
-  }
-  if (data && data.movies) {
-    return data.movies.map((m) => <h1>{m.id}</h1>);
-  }
+  const { loading, data } = useQuery(GET_MOVIES);
+  return (
+    <Container>
+      <Header>
+        <Title>Movie-App2</Title>
+        <Subtitle>Apollo GraphQL</Subtitle>
+      </Header>
+      {loading && <Loading>Loading...</Loading>}
+      {!loading &&
+        data.movies &&
+        data.movies.map((m) => <Movie key={m.id} id={m.id} />)}
+    </Container>
+  );
+  // if (loading) {
+  //   return "loading...";
+  // }
+  // if (data && data.movies) {
+  //   return data.movies.map((m) => <h1>{m.id}</h1>);
+  // }
 };
