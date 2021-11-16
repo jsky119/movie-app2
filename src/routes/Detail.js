8d17964336e9a0b0e283a0937e23d3ead63af89c
jsky119 <87289235+jsky119@users.noreply.github.com>
2021-11-16 20:15:03 +0900

    Feat: Update Detail.js
    1. add GET_MOVIE query
    2. add useParams hooks
    3. add loading and return title code

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,3 +1,28 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { gql, useQuery } from "@apollo/client";
 
-export default () => "Detail";
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      summary
+    }
+  }
+`;
+
+export default () => {
+  const { id } = useParams();
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id: +id },
+  });
+  if (loading) {
+    return "loading";
+  }
+  if (data && data.movie) {
+    return data.movie.title;
+  }
+  return "Detail";
+};
