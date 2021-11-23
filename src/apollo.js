d062ff0920498275bcb0965265d9cf779c2af99d
jsky119 <87289235+jsky119@users.noreply.github.com>
2021-11-23 13:44:45 +0900

    Feat: Add type isLiked

--- src/apollo.js
+++ src/apollo.js
@@ -3,6 +3,11 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
   uri: "http://localhost:4000/",
   cache: new InMemoryCache(),
+  resolvers: {
+    MOvie: {
+      isLiked: () => false,
+    },
+  },
 });
 
 export default client;
